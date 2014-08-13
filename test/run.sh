1a70457da9a34717c2d75f6c1a54c0ae4e362b82
Anilkumar Gingade <agingade@perth.gemstone.com>
2014-08-12 18:29:59 -0700

    adding test run script.

--- test/run.sh
+++ test/run.sh
@@ -1,15 +1,8 @@
 BUILD=~/gem/cedar/build
-CP=.:$BUILD/product/lib/gemfire.jar:$BUILD/product/lib/antlr.jar:$BUILD/tests/classes
-javac -cp $CP TurbineData.java
-javac -cp $CP AvgTurbinePower.java
-javac -cp $CP AvgPowerCalculator.java
-javac -cp $CP TurbineAnalyzer.java
-javac -cp $CP Server.java
+CP=.:$BUILD/product/lib/gemfire.jar:$BUILD/product/lib/antlr.jar:./lib/stream.jar:./classes
+rm -rf classes
+mkdir classes
+javac -cp $CP *.java -d classes
 
-if [ $1 == "server" ]; then
- echo "Starting Server..."
- java -Xms1500m -Xmx1500m -cp $CP Server
-else
- echo "Starting Client..."
-# java -cp $CP Client
-fi
+echo "## Starting Server..."
+java -Xms1500m -Xmx1500m -cp $CP Server
